e2d01c502718b22ebf27f80b0805a5d1af5f174c
Juan Carlos Campos <rapidclimate@gmail.com>
2018-06-20 23:15:31 -0500

    Initial commit

---
+++ src/main/java/hola/crayola/Controlador.java
@@ -0,0 +1,16 @@
+package hola.crayola;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class Controlador {
+
+    @GetMapping("/")
+    public String hola(){
+        return "hola";
+    }
+}
+
---
+++ src/main/java/hola/crayola/CrayolaApplication.java
@@ -0,0 +1,12 @@
+package hola.crayola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrayolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrayolaApplication.class, args);
+	}
+}
